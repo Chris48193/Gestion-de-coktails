84c6e9fb905c168e25669281fdec6fd5d9b05351
Christopher Yepmo <ch.yepmo@eurice.eu>
2021-12-09 14:34:07 +0100

    Removing already added headers in files

--- contenuIndex.php
+++ contenuIndex.php
@@ -1,103 +1,93 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head> -->
-<body>
+
   
-    <!-- MAIN -->
-    <main>  
-        <div class="container-fluid">
+<!-- MAIN -->
+<main>  
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-2 border border-top-0">
           <div class="row">
-            <div class="col-2 border border-top-0">
-              <div class="row">
-                <span class="header-coktail">Liste des Aliments</span>
-              </div>
-              <!-- Listes de liens dirigeant au menu -->
-              <div class="row">
-                <a href="#">Aliment</a>
-              </div>
-              <!-- Liste des sous-categories -->
-              <div class="row">
-                <span >Sous-categories: <span>
-              </div>
-              <?php
-                $sousCatExist = false;
-                if (isset($_GET['superCat']) && isset($_GET['element'])) {
-                  $superCat = $_GET['superCat'];
-                  $element = $_GET['element'];
-                  if($superCat != null && $element != null){
-    
-                    $sousCat = calculerSousCategories($superCat, $element);
-                    $affichage = '<div class="row">
-                                    <a href="#">Aliment</a>
-                                  </div>
-                                    <!-- Liste des sous-categories -->
-                                  <div class="row">
-                                    <span >Sous-categories: <span>
-                                  </div>
-                                  <div class="row">
-                                    <ul class="nav nav-pills flex-column">';
-                    
-                    if(count($sousCat) != 0){
-                      $sousCatExist = true;
-                      foreach($sousCat as $cat){
-                        $affichage .= "<li class=nav-item>
-                                         <a class='nav-link' href=index.php?p=contenuIndex&superCat=$element&element=$cat>$cat</a>
-                                       </li>";
-                      }
-                      $affichage .=  ' </ul>
-                                    </div>'; 
-                      echo $affichage;   
-                    }       
+            <span class="header-coktail">Liste des Aliments</span>
+          </div>
+          <!-- Listes de liens dirigeant au menu -->
+          <div class="row">
+            <a href="#">Aliment</a>
+          </div>
+          <!-- Liste des sous-categories -->
+          <div class="row">
+            <span >Sous-categories: <span>
+          </div>
+          <?php
+            $sousCatExist = false;
+            if (isset($_GET['superCat']) && isset($_GET['element'])) {
+              $superCat = $_GET['superCat'];
+              $element = $_GET['element'];
+              if($superCat != null && $element != null){
+
+                $sousCat = calculerSousCategories($superCat, $element);
+                $affichage = '<div class="row">
+                                <a href="#">Aliment</a>
+                              </div>
+                                <!-- Liste des sous-categories -->
+                              <div class="row">
+                                <span >Sous-categories: <span>
+                              </div>
+                              <div class="row">
+                                <ul class="nav nav-pills flex-column">';
+                
+                if(count($sousCat) != 0){
+                  $sousCatExist = true;
+                  foreach($sousCat as $cat){
+                    $affichage .= "<li class=nav-item>
+                                      <a class='nav-link' href=index.php?p=contenuIndex&superCat=$element&element=$cat>$cat</a>
+                                    </li>";
                   }
+                  $affichage .=  ' </ul>
+                                </div>'; 
+                  echo $affichage;   
+                }       
+              }
 
-                } else {
-                  echo  '<div class="row">
-                          <ul class="nav nav-pills flex-column">
-                            <li class="nav-item">
-                              <a class="nav-link" href="index.php?p=contenuIndex&superCat=Aliment&element=Fruit">Fruit</a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-link" href="index.php?p=contenuIndex&superCat=Aliment&element=Legume">Légumes</a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-link" href="index.php?p=contenuIndex&superCat=Aliment&element=Epices">Epices</a>
-                            </li>
-                            <li class="nav-item">
-                              <a class="nav-link" href="index.php?p=contenuIndex&superCat=Aliment&element=Agrumes">Agrumes</a>
-                            </li>
-                          </ul>
-                        </div>';
-                }
-              ?>    
-            </div>
-            <div class="col-10" id="display-zone">
-              <div class="row-0">
-                <div class="text-center">
-                  <h4 class="mt-1 mb-2">Vos recettes de coktails en details...</h4>
-                  <p>Bonne préparation!</p>
-                </div>
-              </div>
-
-              <!-- Affichage des recettes selon la catégorie sélectionné | Génération avec php -->
-              <?php
-                if($sousCatExist) {
-                  $coktailsExtrait = extraireCoktails($superCat, $element);
-                  echo afficherListeRecettes($coktailsExtrait);
-
-                } else {
-                  echo afficherListeRecettes($tableau);
-                }              
-              ?>
-              <!-- Fin de la génération PHP -->
+            } else {
+              echo  '<div class="row">
+                      <ul class="nav nav-pills flex-column">
+                        <li class="nav-item">
+                          <a class="nav-link" href="index.php?p=contenuIndex&superCat=Aliment&element=Fruit">Fruit</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="index.php?p=contenuIndex&superCat=Aliment&element=Legume">Légumes</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="index.php?p=contenuIndex&superCat=Aliment&element=Epices">Epices</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="index.php?p=contenuIndex&superCat=Aliment&element=Agrumes">Agrumes</a>
+                        </li>
+                      </ul>
+                    </div>';
+            }
+          ?>    
+        </div>
+        <div class="col-10" id="display-zone">
+          <div class="row-0">
+            <div class="text-center">
+              <h4 class="mt-1 mb-2">Vos recettes de coktails en details...</h4>
+              <p>Bonne préparation!</p>
             </div>
           </div>
-          <hr class="d-sm-none">
+
+          <!-- Affichage des recettes selon la catégorie sélectionné | Génération avec php -->
+          <?php
+            if($sousCatExist) {
+              $coktailsExtrait = extraireCoktails($superCat, $element);
+              echo afficherListeRecettes($coktailsExtrait);
+
+            } else {
+              echo afficherListeRecettes($tableau);
+            }              
+          ?>
+          <!-- Fin de la génération PHP -->
         </div>
-      </main>
-</body>
-<!-- </html> -->
+      </div>
+      <hr class="d-sm-none">
+    </div>
+  </main>
--- login.php
+++ login.php
@@ -1,15 +1,3 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
-    <title>Document</title>
-</head>
-<body> -->
     <section class="h-100 gradient-form" style="background-color: #eee;">
         <div class="container py-5 h-100">
           <div class="row d-flex justify-content-center align-items-center h-100">
@@ -60,6 +48,4 @@
             </div>
           </div>
         </div>
-      </section>   
-<!-- </body>
-</html> -->
+      </section>
