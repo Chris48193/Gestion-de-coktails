67f4d0a76459e71bee909f35171660565706eafd
Christopher Yepmo <ch.yepmo@eurice.eu>
2021-12-09 14:10:18 +0100

    Removing search functionality on keyup but adding it on click

--- index.php
+++ index.php
@@ -42,8 +42,8 @@ session_start();
 
 
         <form class="d-flex">
-          <input class="form-control ms-2" type="text" placeholder="Chercher un aliment" id="search-coktail" >
-          <button class="btn border ms-2" type="button">
+          <input class="form-control ms-2" type="text" placeholder="Chercher un aliment" id="search-field">
+          <button class="btn border ms-2" type="button" id="search-coktail">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-search" viewBox="0 0 16 16">
               <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
             </svg>
@@ -109,8 +109,8 @@ session_start();
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <script>
       $(document).ready(function(){
-          $('#search-coktail').keyup(function(){
-              var entry = $(this).val();
+          $('#search-coktail').click(function(){
+              var entry = $('#search-field').val();
               
               if(entry != "") {
                   $.ajax({
